57fd3e370e028b2d6d05fe90acb259b68ef25d00
John Ludlow <john.ludlow@halfords.co.uk>
2026-01-30 10:53:55 +0000

    format

--- TerrainGeneration2D.UnitTests/Core/Mapping/HeuristicsSelectionTests.cs
+++ TerrainGeneration2D.UnitTests/Core/Mapping/HeuristicsSelectionTests.cs
@@ -71,7 +71,7 @@ public sealed class HeuristicsSelectionTests
       Point.Zero,
       new WfcWeightConfiguration(),
       heuristics);
-      
+
     var poss = MappingTestHelpers.GetPrivateField<HashSet<int>?[][]>(wfc, "_possibilities");
     var output = MappingTestHelpers.GetPrivateField<int[][]>(wfc, "_output");
     Assert.NotNull(poss);
